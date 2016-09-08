e65a408518548e17a30aefd5d5396995d042d2e7
Greg Perlman <gperlman27@gmail.com>
2016-09-08 12:22:12 -0400

    add time entries model

--- Week4/timetracking/app/models/project.rb
+++ Week4/timetracking/app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  has_many :time_entries
+
+
 end
--- Week4/timetracking/db/seeds.rb
+++ Week4/timetracking/db/seeds.rb
@@ -6,7 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-4.times { Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
-Project.create!(name: 'Time tracking app')
-Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
-}
+project_1 = Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
+project_2 = Project.create!(name: 'Time tracking app')
+project_3 = Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
+
+project_1.time_entries.create!(hours: 1, minutes: 32)
+project_1.time_entries.create!(hours: 3, minutes: 34)
+project_1.time_entries.create!(hours: 0, minutes: 47)
+project_1.time_entries.create!(hours: 2, minutes: 1)
+project_1.time_entries.create!(hours: 3, minutes: 10)
+project_1.time_entries.create!(hours: 10, minutes: 0)
+
+project_2.time_entries.create!(hours: 5, minutes: 2)
+project_2.time_entries.create!(hours: 2, minutes: 52)
+project_2.time_entries.create!(hours: 1, minutes: 32)
+project_2.time_entries.create!(hours: 4, minutes: 22)
+
+project_3.time_entries.create!(hours: 0, minutes: 12)
+project_3.time_entries.create!(hours: 8, minutes: 2)
+
